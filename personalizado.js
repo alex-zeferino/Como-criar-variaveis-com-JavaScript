43689bdabdb378db584cac26676b0b4dd8d3b57b
Alexsander Zeferino <63205622+alex-zeferino@users.noreply.github.com>
2023-08-16 23:47:01 -0300

    Add files via upload

---
+++ personalizado.js
@@ -0,0 +1,7 @@
+var nome_aluno = "Cesar";
+var idadeAluno = 27;
+var Curso1 = "Curso de JavaScript";
+
+document.write("O aluno " + nome_aluno + ", tem a idade de " + idadeAluno + ". Esta inscrito no " + Curso1);
+
+/*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar*/
